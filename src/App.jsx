27f87b549b7d73627d6e906d3625a0e89dc491c8
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-22 12:58:32 +0800

    Liked Exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-
-import { useState, userState } from "react"
-import CountComponent from "./components/CountComponent"
-import ChildComponent from "./components/ChildComponent"
-
+import LikeComponent from "./components/LikeComponent"
 
 function App() {
 
-  const [count, setCount] = useState(0) //[statename, set+set_name]  best practice 
-
-  const handleIncrement = () => {
-    setCount((prevState) => prevState + 1)
-  }
-
-  const handleDecrement = () => {
-    setCount((prevState) => prevState - 1)
-  }
   return (
     <>
-      <CountComponent 
-      count={count} 
-      handleIncrement={handleIncrement}
-      handleDecrement={handleDecrement}/>
-
-      <br />
-      <hr />
-      <br />
-
-      <ChildComponent>
-        <h1>This in a heading!</h1>
-        <p>This is a sample paragraph.</p>
-      </ChildComponent>
+      <LikeComponent onClick={() => console.log("Clicked")}/>
     </>
   )
 }
---
+++ src/components/LikeComponent.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+
+const LikeComponent = ({ onClick }) => {
+    const [isLiked, setIsLiked] = useState(false)
+    const handleLiked = () => {
+        setIsLiked(!isLiked)
+        onClick()
+    }
+
+  return (
+    <div>   
+        {isLiked ? 
+        <AiFillHeart color="red" onClick={handleLiked}/> 
+        :  
+        <AiOutlineHeart color="red" onClick={handleLiked}/>}
+    </div>
+  )
+}
+
+export default LikeComponent
