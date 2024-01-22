9b80a1fcc6fd4fdcfde540b97d78cffd11e150bb
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-22 11:29:33 +0800

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(event.target.elements.fname.value)
+    console.log(event.target.elements.lname.value)
+  }
+
   return (
     <>
-      <input type="text" name="fname" onChange={(event) => console.log(event.target.value)} />
+      <form className="w-25" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <input type="text" name="fname" placeholder="Enter first name" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <input type="text" name="lname" placeholder="Enter last name" className="form-control" />
+        </div>
+        <button className="btn btn-success" type="submit">Submit Form</button>
+      </form>
     </> 
   )
 }
