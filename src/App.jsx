dbb0bffb4bdaecb725cccbb0f34b6b171de64d7b
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-22 12:30:31 +0800

    Working with React State

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const [count, setCount] = useState(0) //[statename, set+set_name]
   const [name, setName] = useState("Devdotcode")
-  const [isChecked, setIsCheckd] = useState(false)
+  const [isChecked, setIsChecked] = useState(false)
   const [fruits, setFruits] = useState(["Apple", "Mango", "Durian"])
   const [person, setPerson] = useState({name: "Justin", age: 21})
 
@@ -22,12 +22,42 @@ function App() {
     }
   }
 
+  const handleShowFruits = () => {
+    setIsChecked(!isChecked)
+  }
+
+  const handleAddFruit = () => {
+    setFruits([...fruits, "Pineapple"])
+  }
+
+  const handleRemoveFruit = (fruitIndex) => {
+      const filteredFruits = fruits.filter((fruit, index) => index !== fruitIndex 
+    )
+
+    setFruits(filteredFruits)
+  }
+
   return (
     <>
       <h1>Count: {count}</h1>
-      <button onClick={handleIncrement}>Increment</button>
-      
+      <button onClick={handleIncrement}>Increment</button>      
       <button onClick={handleDecrement}>Decrement</button>
+      <hr />
+      <div>
+        <label>Show</label>
+        <input type="checkbox" onChange={handleShowFruits} checked={isChecked} />
+      </div>
+      <button onClick={handleAddFruit}>Add Fruit</button>
+      <ul className="list-group">
+        {isChecked && fruits.map((fruit, index) => {
+          return (
+            <div key={index} >
+              <li className="list-group-items">{fruit}</li>
+              <button className="btn btn-danger" onClick={() => handleRemoveFruit(index)}>Remove Fruits</button>
+            </div>
+          )
+        })}        
+      </ul>      
     </>
   )
 }
