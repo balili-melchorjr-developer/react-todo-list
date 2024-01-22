530e1681de9a72ec1cb239ebfc393d33ba27b8e3
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-22 12:10:01 +0800

    React State

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import NavbarComponent from "./components/NavbarComponent"
-import "./App.css"
+
+import { useState, userState } from "react"
+
 
 function App() {
 
+  const [count, setCount] = useState(0) //[statename, set+set_name]
+  const [name, setName] = useState("Devdotcode")
+  const [isChecked, setIsCheckd] = useState(false)
+  const [fruits, setFruits] = useState(["Apple", "Mango", "Durian"])
+  const [person, setPerson] = useState({name: "Justin", age: 21})
 
+  const handleIncrement = () => {
+    setCount(prevState  => count + 1)
+  }
+
+  const handleDecrement = () => {
+    if (count <= 0) {
+      alert("No you wont")
+    } else {
+      setCount(prevState  => count - 1)
+    }
+  }
 
   return (
     <>
-    
-      <NavbarComponent />
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      
+      <button onClick={handleDecrement}>Decrement</button>
     </>
   )
 }
