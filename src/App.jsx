b75e869591b0f6eafd511c886eca6848d03360c4
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-23 10:30:00 +0800

    React Hooks - useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import { useEffect, useState } from "react"
+import ThemeButton from "./components/ThemeButton"
+import ThemeProvider from "./components/ThemeProvider"
 
 function App() {
-  const [todos, setTodos] = useState([])
-
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      setTodos(data)
-    })
-  }, [])
-
-  useEffect(() => {
-    console.log("Count:" + count)
-  }, [count])
 
   return (
     <>
-    <button className="btn btn-primary" onClick={() => setCount((prevState) => prevState + 1)}>Increment</button>
-    <ul>
-      {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
-    </ul>
+      <ThemeProvider>
+        <div>
+          <h1>Theme Context</h1>
+        </div>
+        <ThemeButton />
+      </ThemeProvider>
     </>
   )
 }
