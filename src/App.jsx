e56330b7b3814a81ed78537afb16e0404e7aa6e4
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-23 10:07:27 +0800

    React Hook - useEffect + useRef part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [todos, setTodos] = useState([])
+
   const [count, setCount] = useState(0)
-  const inputRef = useRef()
+
   useEffect(() => {
-    inputRef.current.focus()
-    console.log("Count updated")
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      setTodos(data)
+    })
   }, [])
 
-  const handleIncrement = () => {
-    setCount((prevState) => prevState + 1)
-  }
+  useEffect(() => {
+    console.log("Count:" + count)
+  }, [count])
+
   return (
     <>
-      <input type="text" ref={inputRef} />
-      <button onClick={handleIncrement}>Increment</button>
+    <button className="btn btn-primary" onClick={() => setCount((prevState) => prevState + 1)}>Increment</button>
+    <ul>
+      {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
     </>
   )
 }
