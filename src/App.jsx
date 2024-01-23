97e70e1aaea5baecbf1a54747788db3aaf74eb30
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-01-23 11:57:07 +0800

    Final Form Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import { useCallback, useState, useMemo } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const doubleNumber = useMemo(() => {
-    console.log("Computing...")
-    return count * 2
-  }, [count])
+  const inputRef = useRef()
+  const [task, setTask] = useState("")
+  const [taskList, setTaskList] = useState([])
+  const [error, setError] = useState("")
+
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if(task) {
+      setTaskList([...taskList, task])
+      setTask("")
+    } else {
+      setError("Task must no be empty.")
+      inputRef.current.focus()
+    }
+    
+  }
 
   return (
-    <>
-      <p>Double Number: {doubleNumber}</p>      
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>Double</button>
-    </>
+    <div className="container mt-5 d-flex justify-content-center">
+      <div className="border p-4">
+        <h1 className="text-center mb-4">Task App</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input 
+            ref={inputRef}
+            type="text" 
+            className="form-control" 
+            placeholder="Enter a task" 
+            value={task}
+            onChange={(event) => setTask(event.target.value)}/>
+            {error ? <p className="text-danger mt-1">{error}</p> : null}
+          </div>
+          <div className="d-flex justify-content-center mt-4">
+            <button type="submit" className="btn btn-primary">Add Task</button>            
+          </div>
+        </form>
+        <ul className="list-group mt-3">
+          {taskList.map((task, index) => <li key={index} className="list-group-item">{task}</li>)}
+        </ul>
+      </div>
+    </div>
   )
 }
 
